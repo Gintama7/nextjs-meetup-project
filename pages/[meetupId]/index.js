5f9c21703b95f553a846a7f9067d9bff6d10a5d8
Gintama7 <43084982+Gintama7@users.noreply.github.com>
2023-12-01 10:48:46 +0530

    detail pages fixed

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,50 +1,59 @@
-import { Fragment } from "react"
+
+import { MongoClient, ObjectId } from "mongodb";
 import MeetupDetail from "../../components/meetups/MeetupDetail"
 
 
-const MeetupDetails = () => {
-  return (<MeetupDetail image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Albertville_place_de_l%27Europe.JPG/1280px-Albertville_place_de_l%27Europe.JPG"
-    title="A First Meetup"
-    address="Some street 5, some city"
-    description="The meetup description"
+const MeetupDetails = (props) => {
+  return (<MeetupDetail image={props.meetupData.image}
+    title={props.meetupData.title}
+    address={props.meetupData.address}
+    description={props.meetupData.description}
     />
   )
 }
 
 export async function getStaticPaths(){
+
+
+  const client =await MongoClient.connect('mongodb+srv://elnino:bitwcr7@cluster0.oafqt3b.mongodb.net/meetups');
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const meetups = await meetupsCollection.find({},{_id:1}).toArray();
+
+
   return{
     fallback:false,
-    paths:[
-      {
-        params:{
-          meetupId:'m1',
-        }
-      },
-      {
-        params:{
-          meetupId:'m2',
-        }
-      },
-      {
-        params:{
-          meetupId:'m3',
-        }
-      }
-    ]
+    paths:meetups.map((meetup)=>({
+      params: {meetupId: meetup._id.toString()},
+    })),
   }
 }
 
 export async function getStaticProps(context){
+
+
   const meetupId = context.params.meetupId;
+
+  const client =await MongoClient.connect('mongodb+srv://elnino:bitwcr7@cluster0.oafqt3b.mongodb.net/meetups');
+  const db = client.db();
+
+  const meetupsCollection = db.collection('meetups');
+
+  const selectedMeetup = await meetupsCollection.findOne({_id:new ObjectId(meetupId),});
+
+  client.close();
+
   return{
     props:{
       meetupData:{
-        id: meetupId,
-        image:"https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Albertville_place_de_l%27Europe.JPG/1280px-Albertville_place_de_l%27Europe.JPG",
-    title:"A First Meetup",
-    address:"Some street 5, some city",
-    description:"The meetup description",
-      }
+         id:selectedMeetup._id.toString(),
+         title:selectedMeetup.title,
+         address:selectedMeetup.address,
+         image:selectedMeetup.image,
+         description:selectedMeetup.description
+      },
     }
   }
 }
